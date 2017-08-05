054676837f0c3c0deb9706e0d9f0658ab46bebde
Lordinel Grajo <lordinel@gmail.com>
2017-08-05 14:32:24 +0800

    Set bug tracker to version 4.14

--- buglist.php
+++ buglist.php
@@ -1,10 +1,11 @@
 <?php
 $PAGE_NAME = 'Bug Tracker';
 
-$stableBuild = array('version' => '4.13',
-					 'label'   => '4.13');
+$stableBuild = array('version' => '4.14',
+					 'label'   => '4.14');
 
-$builds = array('4.12'   => '4.12',
+$builds = array('4.13'   => '4.13',
+				'4.12'   => '4.12',
 				'4.11'   => '4.11',
 				'4.10'   => '4.10',
 				'4.9'    => '4.9',
